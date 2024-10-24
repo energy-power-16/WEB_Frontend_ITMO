fabd757bffab26e1d54b98fc65b55fca5b225c51
energy-power-16 <117775131+energy-power-16@users.noreply.github.com>
2024-10-24 14:50:24 +0300

    Add files via upload

--- LB4_1/scripts/script.js
+++ LB4_1/scripts/script.js
@@ -1,44 +1,37 @@
 'use strict';
 
-let start = new Date;
-console.log(+start);
-
-(function () {
-    window.addEventListener('load', function () {
-        let end = new Date;
-        console.log(+end);
-        const loadTimeSpan = document.querySelector('.load-dur');
-        if (loadTimeSpan) {
-            loadTimeSpan.textContent = (end - start) / 1000;
-        }
-    });
-})();
-
-(function () {
-    window.addEventListener('DOMContentLoaded', function () {
-        makeMenuItemActive2();
-        let end = new Date;
-        const loadTimeSpan = document.querySelector('.dom-content-loaded-dur');
-        if (loadTimeSpan) {
-            loadTimeSpan.textContent = (end - start) / 1000;
-        }
-    });
-})();
-
-function makeMenuItemActive1() {
-    let pageName = document.location.pathname;
-    let elements = document.querySelectorAll('.header__link');
-    for (const el of elements) {
-        if (el.getAttribute('href') === '.' + pageName) {
-            el.classList.add('header__link__active');
+(function() {
+    let start = new Date;
+    
+    (function () {
+        window.addEventListener('load', function () {
+            let end = new Date;
+            const loadTimeSpan = document.querySelector('.load-dur');
+            if (loadTimeSpan) {
+                loadTimeSpan.textContent = (end - start) / 1000;
+            }
+        });
+    })();
+    
+    (function () {
+        window.addEventListener('DOMContentLoaded', function () {
+            makeMenuItemActive();
+            let end = new Date;
+            const loadTimeSpan = document.querySelector('.dom-content-loaded-dur');
+            if (loadTimeSpan) {
+                loadTimeSpan.textContent = (end - start) / 1000;
+            }
+        });
+    })();
+    
+    function makeMenuItemActive() {
+        const loc = document.location;
+        let pageHref = loc.origin + loc.pathname;
+        let elements = document.querySelectorAll('.header__link');
+        for (const el of elements) {
+            if (el.href === pageHref) {
+                el.classList.add('header__link__active');
+            }
         }
     }
-}
-
-function makeMenuItemActive2() {
-    let pageName = document.location.pathname;
-    let el = document.querySelector(`.header__link[href=".${pageName}"]`);
-    if (el) {
-        el.classList.add('header__link__active');
-    }
-}
+})();
