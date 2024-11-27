ea67546485620e08d630cf3925eb94a4d712c77b
energy-power-16 <117775131+energy-power-16@users.noreply.github.com>
2024-11-27 18:49:13 +0300

    lab7 fix

--- LB5-6/scripts/comments.js
+++ LB5-6/scripts/comments.js
@@ -8,18 +8,33 @@
     btn.addEventListener('click', btnLoadClick);
   }
 
-  function btnLoadClick(){
+  function btnLoadClick() {
+    // Зададим вместо текста для кнопки "OK" текст "Конечно"
+    vex.dialog.buttons.YES.text = 'Конечно';
+
+    // Зададим вместо текста для кнопки "Cancel" текст "Ни в коем разе"
+    vex.dialog.buttons.NO.text = 'Ни в коем разе';
+
+    // Меняем стиль модальных окон на более подходящий для этого сайта
+    vex.defaultOptions.className = 'vex-theme-flat-attack';
+
+
+    // confirm открывает модальное окно с 2мя кнопками:
+    // OK и Cancel.
+    // В качестве параметра передаём объект со следующими свойствами:
+    // message: какой текст отобразить в модальном окне,
+    // callback: какую функцию вызвать после того, как пользователь
+    // нажмёт OK.
     vex.dialog.confirm({
       message: 'Вы точно желаете загрузить данные по сети?',
-      callback: function (value)
-      {
-          if (value){
-            loadComments();
-          }
+      callback: function (value) {
+        if (value) {
+          loadComments();
+        }
       }
     })
   }
-
+  
   async function loadComments() {
     const loader = document.querySelector('.loader');
     const erDiv = document.querySelector('.comments-error');
@@ -34,7 +49,9 @@
       const postId = Math.floor(Math.random() * 100) + 1;
       const resp = await fetch(`https://jsonplaceholder.typicode.com/comments?postId=${postId}`);
       comments = await resp.json();
-      vex.dialog.alert(`Данные успешно загружены для post id = ${postId}`)
+      // alert отображает модальное окно с тестом,
+      // указанным в качестве аргумента
+      vex.dialog.alert(`Данные успешно загружены для post id = ${postId}`);
     } catch (er) {
       erDiv.textContent = `Ошибка: ${er.message}`;
       erDiv.classList.remove('none');
